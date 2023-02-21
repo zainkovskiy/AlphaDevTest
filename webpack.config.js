4511e56f4cba66ede4efc6fb7258f39cf9550c08
Anton Zainkovskiy <Piton1808@gmail.com>
2023-02-21 21:49:32 +0100

    correct style

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js'
   },
   resolve: {
